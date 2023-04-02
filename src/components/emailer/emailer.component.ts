9f906eba3ad00c877408a086ea306d13dee9ad50
David Eastmond <davebuck1981@gmail.com>
2023-04-02 17:10:06 -0400

    new routes and validators

--- src/components/emailer/emailer.component.ts
+++ src/components/emailer/emailer.component.ts
@@ -1,14 +1,14 @@
 import { Request, Response, Router } from "express";
-import { PasswordRecoveryEmail } from "../../data/emails/password-recovery-email/password-recovery-email";
-import { Emailer } from "../../utils/emailer/emailer";
-import { recoveryEmailValidator } from "./validators/emailer-validators";
+import { PasswordRecoveryEmail, PasswordChangedEmail, WelcomeEmail, VerificationCodeEmail } from "../../data/emails";
+import { emailer } from "../../utils/emailer/emailer";
+
+import { passwordChangedEmailValidator, recoveryEmailValidator, welcomeEmailValidator, verificationCodeEmailValidator } from "./validators/emailer-validators";
 
 async function postSendPasswordRecoveryEmail (req: Request, res: Response) {
   // Send a password recovery e-mail
-  const { recipient, name, recoveryURL } = req.body;
+  const { recipient, name, recoveryURL, adminEmail } = req.body;
   try {
-    const emailer = new Emailer();
-    await emailer.sendEmail(new PasswordRecoveryEmail(recipient, name, recoveryURL));
+    await emailer.sendEmail(new PasswordRecoveryEmail(recipient, adminEmail, name, recoveryURL));
     return res.status(200).send({ msg: 'OK'})
   } catch (err: any) {
     res.status(500).send({
@@ -17,8 +17,47 @@ async function postSendPasswordRecoveryEmail (req: Request, res: Response) {
   }
 }
 
+async function postSendPasswordChangedEmail(req: Request, res: Response) {
+  // Send notification e-mail that password changed
+  const { recipient, name } = req.body;
+  try {
+    await emailer.sendEmail(new PasswordChangedEmail(recipient, name));
+    return res.status(200).send({ msg: 'OK'});
+  } catch (err: any) {
+    return res.status(500).send({
+      err: err
+    })
+  }
+}
+
+async function postSendWelcomeEmail(req: Request, res: Response) {
+  const { applicationId, recipient, name, adminEmail } = req.body;
+  try {
+    await emailer.sendEmail(new WelcomeEmail(recipient, name, applicationId, adminEmail));
+    return res.status(200).send({ msg: 'OK'})
+  } catch (err: any) {
+    return res.status(500).send({
+      err: err
+    })
+  }
+}
+
+async function postSendVerificationCodeEmail(req: Request, res: Response) {
+  const { code, recipient } = req.body;
+  try {
+    await emailer.sendEmail(new VerificationCodeEmail(recipient, code));
+  } catch (err: any) {
+    return res.status(500).send({
+      err: err
+    })
+  }
+}
+
 export default function(app: Router ) {
   app.post("/email/recovery-email", recoveryEmailValidator, postSendPasswordRecoveryEmail);
+  app.post("/email/password-changed-notification", passwordChangedEmailValidator, postSendPasswordChangedEmail);
+  app.post("/email/welcome-email", welcomeEmailValidator, postSendWelcomeEmail);
+  app.post("/email/verification-code", verificationCodeEmailValidator, postSendVerificationCodeEmail)
 }
 
 console.info("Emailer component registered.");
--- src/components/emailer/validators/emailer-validators.ts
+++ src/components/emailer/validators/emailer-validators.ts
@@ -3,7 +3,29 @@ import { validationGuard } from '../../../middleware/validation-guard';
 
 export const recoveryEmailValidator = [
   body('recipient').exists().isEmail(),
+  body('adminEmail').exists().isEmail(),
   body('name').exists(),
   body('recoveryURL').exists().isString(),
   validationGuard,
+]
+
+export const passwordChangedEmailValidator = [
+  body('recipient').exists().isEmail(),
+  body('adminEmail').exists().isEmail(),
+  body('name').exists(),
+  validationGuard,
+]
+
+export const welcomeEmailValidator = [
+  body('recipient').exists().isEmail(),
+  body('adminEmail').exists().isEmail(),
+  body('name').exists(),
+  body('applicationId').exists(),
+  validationGuard
+]
+
+export const verificationCodeEmailValidator = [
+  body('recipient').exists().isEmail(),
+  body('code').exists(),
+  validationGuard
 ]
